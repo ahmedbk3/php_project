1216835d13e3844f95053d05d3bc3a29456040a2
dali-333 <abdellatifmouhamedali@gmail.com>
2025-04-09 21:43:26 +0100

    FirePokemon Completed

--- classes/PokemonFeu.php
+++ classes/PokemonFeu.php
@@ -1,16 +1,28 @@
 <?php
-include 'Pokemon.php'; // Ensure to include the base class
+require_once 'Pokemon.php'; // Ensure to include the base class
+require_once 'Pokemon.php'; // Ensure to include the AttackPokemon class
 
 class PokemonFeu extends Pokemon {
     public function attack($opponent) {
         $attackPoints = $this->attackPokemon->getAttackPoints();
+        $specialAttack = $this->attackPokemon->isSpecialAttack();
+
+        $mult = 1;
+        
         if ($opponent instanceof PokemonPlante) {
-            $attackPoints *= 2; // Fire is strong against Plant
-        } elseif ($opponent instanceof PokemonEau) {
-            $attackPoints *= 0.5; // Fire is weak against Water
+            $mult = 2; // Fire is strong against Plant
+        } elseif ($opponent instanceof PokemonEau || $opponent instanceof PokemonFeu) {
+            $mult = 0.5; // Fire is weak against Water & Fire
         }
-        $opponent->hp -= $attackPoints;
-        return $attackPoints;
+
+        if ($specialAttack) {
+            $attackPoints *= $this->attackPokemon->getSpecialAttack();
+            echo "<strong>" . $this->name . "</strong> used a SPECIAL attack of <strong>" .  $attackPoints . "</strong> against <strong>" . $opponent->getName() . "</strong>!<br>";
+        } else {
+            echo "<strong>" . $this->name . "</strong> used a normal attack of <strong>" .  $attackPoints . "</strong> against <strong>" . $opponent->getName() . "</strong>!<br>";
+        }
+
+        $opponent->recieveDamage($attackPoints * $mult);
     }
 }
 ?>
